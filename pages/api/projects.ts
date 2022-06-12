a7490efb78e651a37840b15db3d22cfc45dbdf16
clarade <cdesperben@yahoo.fr>
2022-06-13 01:30:48 +0200

    fix(build): delete useless api route

--- pages/api/projects.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const { data } = await strapi.find('projects', {
-		populate: ['Images', 'Thumbnail'],
-	});
-
-	return res.status(200).json(data);
-};
