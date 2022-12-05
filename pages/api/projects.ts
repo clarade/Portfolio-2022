26f6d310a7f3808b7897cb713b51283e903a7edf
clarade <cdesperben@yahoo.fr>
2022-12-05 16:48:48 +0100

    declared strapi due to changes of the stack

--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const strapi = require('@strapi/strapi');
+
 	const { data } = await strapi.find('projects', {
 		populate: ['Images', 'Thumbnail'],
 	});
